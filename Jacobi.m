fd6880207d4954c7089d8663a2ac89ac805be2b0
Simone Benitozzi <72170256+simonebenitozzi@users.noreply.github.com>
2022-03-23 18:10:46 +0100

    Add files via upload

---
+++ Jacobi.m
@@ -0,0 +1,18 @@
+N = 1000, n = 1, toll = 10^-6;;
+
+A = [3 1 0 0 0; 1 3 1 0 0; 0 1 3 1 0; 0 0 1 3 1; 0 0 0 1 3];
+D = diag(diag(A));
+b = [1; 1; 1; 1; 1];
+
+x = rand(5, 1)
+xPrev = x;
+x = D\(D-A)*xPrev + D\b;
+
+while(n<=N & (abs(x - xPrev) > toll))
+    xPrev = x;
+    x = D\(D-A)*xPrev + D\b;
+    n = n+1;
+end
+
+A*x
+
